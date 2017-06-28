8351e7c1f6468ca597aca778811c264077f95c5a
Timon Orawski <timon@orawski.com>
2017-06-28 15:12:20 -0400

    Lots of work

--- src/JolieBrise.ino
+++ src/JolieBrise.ino
@@ -2,9 +2,16 @@
 #include <Adafruit_BMP085.h>
 #include <Adafruit_DHT.h>
 #include <Adafruit_HMC5883.h>
-
+//#include <google-maps-device-locator.h>
+#include "TinyGPS.h"
 #include "defs.h"
 
+// GPS
+int loopsSinceUpdate = 0;
+TinyGPS gps;
+char szInfo[64];
+// Every 15 minutes
+int debug = 0;
 int loopDelay = defaultLoopDelay;
 bool bmpAvailable = true;
 bool magAvailable = true;
@@ -31,13 +38,16 @@ double latitude;
 double longitude;
 double altitude;
 double magHeading;
-double trueHeading;
+double course;
+int satellites;
+double speed;
 
 double battery1Voltage;
 double battery2Voltage;
 
 double uvIndex;
 // end variables
+
 Calibration calibration;
 
 #define DHTPIN D4
@@ -56,6 +66,9 @@ void initCalibration() {
   calibration.bilgeMax = BILGEDEPTHCALIBRATEDMAX;
   calibration.bilgeMin = BILGEDEPTHCALIBRATEDMIN;
   calibration.bilgeSeriesResistor = BILGEDEPTHSERIESRESISTOR;
+  calibration.locationFrequency = LOCATIONFREQUENCY;
+  calibration.heartbeatIntervalSeconds = NOTIFYINTERVAL;
+  calibration.loopDelaySeconds = LOOPTIME;
   writeCalibration();
 }
 
@@ -66,14 +79,16 @@ void writeCalibration() {
 void printCalibration() {
   char publishString[255];
   sprintf(publishString,
-      "{v: %u, mag: %f, wvMin: %f, wvMax: %f, bMax: %u, bMin: %u, bRes: %u}",
+      "{v: %u, mag: %f, wvMin: %f, wvMax: %f, bMax: %f, bMin: %f, bRes: %u, hb: %u, l: %u}",
       calibration.version,
       calibration.magOffset,
       calibration.windVoltsMin,
       calibration.windVoltsMax,
       calibration.bilgeMax,
       calibration.bilgeMin,
-      calibration.bilgeSeriesResistor);
+      calibration.bilgeSeriesResistor,
+      calibration.heartbeatIntervalSeconds,
+      calibration.loopDelaySeconds);
   Particle.publish("joliebrise/system/calibration", publishString);
 }
 
@@ -105,6 +120,8 @@ void setup() {
     Particle.publish("joliebrise/sensors/hmc5883l/initialized");
     magAvailable =  true;
   }
+  // GPS
+  Serial1.begin(19200);
 
 	Particle.variable("bilgeraw", bilgeReading);
 	Particle.variable("bilgelevel", bilgeLevel);
@@ -122,14 +139,64 @@ void setup() {
 	Particle.variable("longitude", longitude);
 	Particle.variable("altitude", altitude);
 	Particle.variable("magHeading", magHeading);
-	Particle.variable("trueHeading", trueHeading);
+	Particle.variable("course", course);
+  Particle.variable("satellites", satellites);
+  Particle.variable("speed", speed);
+
   Particle.publish("joliebrise/system/setup");
 }
 
 void loop() {
 	update("all");
+  processGPS();
   processData();
-	delay(1000);
+  loopsSinceUpdate++;
+  if ((loopsSinceUpdate * calibration.loopDelaySeconds) >= calibration.heartbeatIntervalSeconds ) {
+    Particle.publish("joliebrise/notify/alive");
+    //Particle.publish("joliebrise/notify/bilgeLevel", String(bilgeLevel));
+    loopsSinceUpdate = 0;
+  }
+	delay(calibration.loopDelaySeconds * 1000);
+}
+
+void processGPS() {
+  bool isValidGPS = false;
+
+    for (unsigned long start = millis(); millis() - start < 1000;){
+        // Check GPS data is available
+        while (Serial1.available()){
+            char c = Serial1.read();
+
+            // parse GPS data
+            if (gps.encode(c))
+                isValidGPS = true;
+        }
+    }
+
+    // If we have a valid GPS location then publish it
+    if (isValidGPS){
+        float lat, lon;
+        unsigned long age;
+
+        gps.f_get_position(&lat, &lon, &age);
+        latitude = lat;
+        longitude = lon;
+        sprintf(szInfo, "%.6f,%.6f", (lat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lat), (lon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lon));
+    }
+    else{
+        // Not a vlid GPS location, jsut pass 0.0,0.0
+        // This is not correct because 0.0,0.0 is a valid GPS location, we have to pass a invalid GPS location
+        // and check it at the client side
+        sprintf(szInfo, "not available");
+    }
+
+    if (debug) {
+      Particle.publish("joliebrise/location", szInfo);
+    }
+    satellites = gps.satellites();
+    speed = gps.f_speed_knots();
+    course = gps.f_course();
+    altitude = gps.f_altitude();
 }
 
 void processData() {
@@ -142,7 +209,9 @@ void processData() {
     Particle.publish("joliebrise/controls/pump/stop");
     pumping = false;
   }
+  //locator.publishLocation();
 }
+
 int update(String cmd) {
 	if (cmd == "all" || cmd == "bilgeLevel") {
 		getBilgeLevel();
@@ -179,6 +248,18 @@ int calibrate(String cmd) {
   if (cmd == "bilgeMax") {
     calibration.bilgeMax = readResistance(waterPin, calibration.bilgeSeriesResistor);
   }
+  int colonPos = cmd.indexOf(":");
+  if (colonPos > -1){
+    if (cmd.substring(0, colonPos) == "heartbeat") {
+      calibration.heartbeatIntervalSeconds = cmd.substring(colonPos+1, cmd.length()).toInt();
+    }
+    if (cmd.substring(0, colonPos) == "loopDelay") {
+      calibration.loopDelaySeconds = cmd.substring(colonPos+1, cmd.length()).toInt();
+    }
+    if (cmd.substring(0, colonPos) == "debug") {
+      debug = cmd.substring(colonPos+1, cmd.length()).toInt();
+    }
+  }
   writeCalibration();
   printCalibration();
   return 1;
@@ -200,7 +281,7 @@ int pumpBilge(String cmd) {
 float readResistance(int pin, int seriesResistance) {
   // Get ADC value.
   float resistance = analogRead(pin);
-  Particle.publish("resist", String(resistance));
+  // Particle.publish("resist", String(resistance));
   // Convert ADC reading to resistance.
   resistance = (4095.0 / resistance) - 1.0;
   resistance = seriesResistance / resistance;
@@ -265,9 +346,5 @@ void getMeteo() {
 }
 
 void getSeaTalk() {
-
-}
-
-void readGPS() {
 
 }
--- src/defs.h
+++ src/defs.h
@@ -5,18 +5,21 @@ const int pumpPin = D3;
 const int defaultLoopDelay = 1000*60*30;
 
 #define USEGPS 1
-int gpsFrequency = 900;
+#define LOCATIONFREQUENCY 900
 
 // definitions for bilge level sensor
 #define BILGEDEPTHSERIESRESISTOR 560;
 #define BILGEDEPTHCALIBRATEDMAX 2048;
 #define BILGEDEPTHCALIBRATEDMIN 300;
 
+#define GOOGLEAPIKEY "AIzaSyDQwI2vfQsYXXyT1OJO9mKqMFKE4iy7s9w"
 #define analogToVolts .004882814;
 #define WINDVOLTAGEMIN 0.4;
 #define WINDVOLTAGEMAX 2.0;
 const int windSpeedMin = 0; // Wind speed in meters/sec corresponding to minimum voltage
 const int windSpeedMax = 32; // Wind speed in meters/sec corresponding to maximum voltage
+#define NOTIFYINTERVAL 6 * 60 * 60
+#define LOOPTIME 10
 
 typedef struct {
   char version;
@@ -26,7 +29,10 @@ typedef struct {
   float bilgeMax;
   float bilgeMin;
   int bilgeSeriesResistor;
-  char future[36];
+  int locationFrequency;
+  int heartbeatIntervalSeconds;
+  int loopDelaySeconds;
+  char future[24];
 } Calibration;
 
 #ifdef GY_87
